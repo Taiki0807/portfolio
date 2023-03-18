03fda691e05850eb659374e4c7eabec8e2696e8d
Taiki0807 <hosokawataiki265@gmail.com>
2023-03-18 19:22:11 +0900

    feat: implement responsive design

--- app/components/pages/Header/Header.tsx
+++ app/components/pages/Header/Header.tsx
@@ -1,16 +1,32 @@
-import { FaGithub } from 'react-icons/fa';
-import { Icon } from '../../parts';
+'use client';
+import { useState } from 'react';
+import { FaGithub, FaBars } from 'react-icons/fa';
 import style from './Header.module.css';
+import { Icon } from '@/app/components/parts';
 
 export const Header = (): JSX.Element => {
+  const [openMenu, setOpenMenu] = useState(false);
+  const MenuHandle = () => {
+    setOpenMenu(!openMenu);
+  };
   return (
     <header className={style.header}>
       <div className={style.header__wrapper}>
         <nav className={style.nav}>
+          <div
+            className={style.checkbtn}
+            onClick={MenuHandle}
+          >
+            <FaBars color="white" />
+          </div>
           <a href="/" className={style.logo}>
             Hosokawa-lab
           </a>
-          <ul className={style.nav__wrapper}>
+          <ul
+            className={`${style.nav__wrapper} ${
+              openMenu ? undefined : style.nav__hidden
+            }`}
+          >
             <li className={style.nav__item}>
               <a href="/">Home</a>
             </li>
