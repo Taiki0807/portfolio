a1492db172d54ae943590ea0fa62c0ef05361262
Taiki0807 <hosokawataiki265@gmail.com>
2023-03-20 01:36:09 +0900

    feat: connect article list to API

--- app/components/pages/Page/Page.tsx
+++ app/components/pages/Page/Page.tsx
@@ -1,19 +1,31 @@
+import style from './Page.module.css';
+import { useArticles } from './useArticles';
 import { AboutCard } from './index';
 import { ArticleCard } from '@/app/components/features';
 import { Title } from '@/app/components/parts';
 
-export const Page = (): JSX.Element => {
+export const Page = async () => {
+  const { Data }: any = await useArticles();
+  if (!Data) return <div>loading...</div>;
   return (
     <div>
       <AboutCard />
       <Title title={'LATEST ARTICLES'} />
-      <ArticleCard
-        title={'test'}
-        url={'/'}
-        image={
-          'https://placehold.jp/320x185.png?text=サンプル'
-        }
-      />
+      <div className={style.articles__item}>
+        {Data.map((data: any, index: number) => {
+          return (
+            <ArticleCard
+              key={`${index}`}
+              title={data.title}
+              url={`/article/${data.id}`}
+              image={
+                data.thumbnail ||
+                'https://placehold.jp/320x185.png?text=No Image'
+              }
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
