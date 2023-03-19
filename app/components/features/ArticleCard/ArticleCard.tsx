ff6562adc4f864aefabbc1383ee5ddba6fe8c484
Taiki0807 <hosokawataiki265@gmail.com>
2023-03-20 01:27:19 +0900

    feat: implement fetching articles from API

--- app/components/features/ArticleCard/ArticleCard.tsx
+++ app/components/features/ArticleCard/ArticleCard.tsx
@@ -10,28 +10,24 @@ interface Props {
 export const ArticleCard = (props: Props): JSX.Element => {
   return (
     <div id="articles" className={style.changeCard}>
-      <ul>
-        <li className={style.changeItem}>
-          <a href={props.url}>
-            <p className={style.itemCat}>{props.title}</p>
-            <Image
-              className={style.img}
-              src={props.image}
-              alt="image"
-              fill
-            />
-            <div className={style.changeItemTxt}>
-              <p className={style.itemTitle}>
-                {props.title}
-              </p>
-              <ul className={style.itemTag}>
-                <li>Next.js</li>
-                <li>Next.js</li>
-              </ul>
-            </div>
-          </a>
-        </li>
-      </ul>
+      <div className={style.changeItem}>
+        <a href={props.url}>
+          <p className={style.itemCat}>{props.title}</p>
+          <Image
+            className={style.img}
+            src={props.image}
+            alt="image"
+            fill
+          />
+          <div className={style.changeItemTxt}>
+            <p className={style.itemTitle}>{props.title}</p>
+            <ul className={style.itemTag}>
+              <li>Next.js</li>
+              <li>Next.js</li>
+            </ul>
+          </div>
+        </a>
+      </div>
     </div>
   );
 };
