859092510889c81b3c2c16f1a8806eac7dd19675
Taiki0807 <hosokawataiki265@gmail.com>
2023-03-25 03:23:24 +0900

    feat: add code to highlight

--- app/components/pages/Article/useArticle.tsx
+++ app/components/pages/Article/useArticle.tsx
@@ -1,5 +1,12 @@
+import { load } from 'cheerio';
+import hljs from 'highlight.js';
 import { getFetcher } from '@/utils/httpClient';
+import 'highlight.js/styles/atom-one-dark.css';
 
+type Tag = {
+  id: number;
+  name: string;
+};
 type Article = {
   id: number;
   category: {
@@ -13,11 +20,22 @@ type Article = {
   thumbnail: string | null;
   lead_text: string;
   created_at: string;
+  updated_at: string;
+  tag: Tag[];
 };
 
 export const useArticle = async (id: string) => {
   const url = `/api/blog/posts/${id}`;
-  const data: Article = await getFetcher(url);
+  const data: Article = await getFetcher(url, 'no-store');
+  const $ = load(data.main_text);
+
+  $('code').each((_, elm) => {
+    const result = hljs.highlightAuto($(elm).text());
+    $(elm).html(result.value);
+    $(elm).addClass('hljs');
+  });
+
+  data.main_text = $.html();
   return {
     Data: data,
   };
