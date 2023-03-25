2a2354e8a95b8a3bbd4b4aefc3812cb8132bfe10
Taiki0807 <hosokawataiki265@gmail.com>
2023-03-25 14:30:42 +0900

    feat: add loader

--- app/components/parts/index.ts
+++ app/components/parts/index.ts
@@ -1,3 +1,4 @@
 export { Button } from './Button';
 export { Icon } from './Icon';
 export {Title} from './Title';
+export {Loader} from './Loader';
---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import { Loader } from './components/parts';
+
+const loading = () => {
+  return (
+    <div>
+      <Loader />
+    </div>
+  );
+};
+
+export default loading;
