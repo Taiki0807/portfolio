47984333dad1f226f6efbc9b8aa51c138d9ed97c
Taiki0807 <hosokawataiki265@gmail.com>
2023-07-01 19:44:45 +0900

    feat: add WorksCard component

--- app/_components/pages/TopPage/components/Page/Page.tsx
+++ app/_components/pages/TopPage/components/Page/Page.tsx
@@ -1,6 +1,8 @@
+import { Works } from '../../../type';
 import { AboutCard } from '../index';
 import style from './Page.module.css';
 import { ArticleCard } from '@/app/_components/features';
+import { WorksCard } from '@/app/_components/features/Card';
 import { Title } from '@/app/_components/parts';
 
 interface Category {
@@ -24,9 +26,13 @@ interface Data {
 
 interface Props {
   data: Data[];
+  works: Works[];
 }
 
-export const Page = ({ data }: Props): JSX.Element => {
+export const Page = ({
+  data,
+  works,
+}: Props): JSX.Element => {
   return (
     <div>
       <AboutCard />
@@ -44,6 +50,24 @@ export const Page = ({ data }: Props): JSX.Element => {
               }
               tag={data.tag}
               category={data.category}
+            />
+          );
+        })}
+      </div>
+      <Title title="Works" />
+      <div className={style.works}>
+        {works.map((data: any, index: number) => {
+          return (
+            <WorksCard
+              key={`${index}`}
+              title={data.title}
+              description={data.description}
+              imageUrl={
+                data.thumbnail ||
+                'https://placehold.jp/320x185.png?text=No Image'
+              }
+              link={`/works/${data.id}`}
+              category={data.category.name}
             />
           );
         })}
