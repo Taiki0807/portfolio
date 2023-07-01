6caecbda46b56603e2f0cc4779531ebfaf2d202c
Taiki0807 <hosokawataiki265@gmail.com>
2023-07-01 19:34:50 +0900

    feat: add works card component

---
+++ app/_components/features/Card/WorksCard/WorksCard.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import style from './WorksCard.module.css';
+
+interface Props {
+  title: string;
+  description: string;
+  imageUrl: string;
+  link: string;
+  category: string;
+}
+
+export const WorksCard = (props: Props): JSX.Element => {
+  return (
+    <div className={style.worksCard}>
+      <Link href={props.link}>
+        <div className={style.image__container}>
+          <Image
+            src={props.imageUrl}
+            alt="Picture of the author"
+            fill
+            className={style.image}
+          />
+        </div>
+        <div className={style.card__item}>
+          <h3>
+            {props.title}
+            <span className={style.tag}>
+              {props.category}
+            </span>
+          </h3>
+          <p>{props.description}</p>
+        </div>
+      </Link>
+    </div>
+  );
+};
