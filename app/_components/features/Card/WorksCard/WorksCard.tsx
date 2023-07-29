86d073586f7276e6a2617df6c1a7e162ed9374cf
Taiki0807 <hosokawataiki265@gmail.com>
2023-07-29 09:57:43 +0900

    feat: add emoji to work thumbnail

--- app/_components/features/Card/WorksCard/WorksCard.tsx
+++ app/_components/features/Card/WorksCard/WorksCard.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { Twemoji } from 'react-emoji-render';
 import style from './WorksCard.module.css';
 
 interface Props {
   title: string;
   description: string;
-  imageUrl: string;
+  imageUrl?: string;
   link: string;
   category: string;
 }
@@ -15,12 +16,20 @@ export const WorksCard = (props: Props): JSX.Element => {
     <div className={style.worksCard}>
       <Link href={props.link}>
         <div className={style.image__container}>
-          <Image
-            src={props.imageUrl}
-            alt="Picture of the author"
-            fill
-            className={style.image}
-          />
+          {props.imageUrl ? (
+            <Image
+              src={props.imageUrl}
+              alt="Picture of the author"
+              fill
+              className={style.image}
+            />
+          ) : (
+            <Twemoji
+              className={style.postCardEmoji}
+              svg
+              text={'📝'}
+            />
+          )}
         </div>
         <div className={style.card__item}>
           <h3>
